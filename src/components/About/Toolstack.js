6cda5322c7dc0cc1df92e4056e2d842f5e32346b
Farhat Sumaiya <Farhattsumaiya@gmail.com>
2024-05-17 15:29:24 -0400

    ..

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,11 +1,1 @@
-// function Toolstack() {
-//   return (
-//     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-//       <Col xs={4} md={2} className="tech-icons">
-//         <SiSlack />
-//       </Col>
-//     </Row>
-//   );
-// }
-
 // export default Toolstack;
